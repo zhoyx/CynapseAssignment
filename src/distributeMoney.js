0c5f5b3ad1e8eac21eb40bd1c9900cbee7ba1bbe
zhoyx <zhouyx2000@gmail.com>
2023-02-13 16:23:09 +0800

    Add Input type validation for distributeMoney

--- src/distributeMoney.js
+++ src/distributeMoney.js
@@ -3,9 +3,12 @@
  * @param {number} n - The number of participants to distribute money to.
  * @param {number} amt - The amount of money in dollars to distribute.
  * @param {number} granularity - Level of granularity in distribution.
- * @returns An array containing the distributed amount to each participant.
+ * @returns {Array} An array containing the distributed amount to each participant.
  */
 function distributeMoney(n, amt, granularity = 100) {
+    if (typeof n !== 'number' || typeof amt !== 'number' || typeof granularity !== 'number') {
+        throw new TypeError('All input parameters must be of type number');
+    }
     let dist = Array(n).fill(1);
     amt *= granularity;
     amt -= n;
