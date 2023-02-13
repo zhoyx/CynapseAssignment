fc28573386cb6b7fe4d598eb914f234ecd1aa2b1
zhoyx <zhouyx2000@gmail.com>
2023-02-13 15:40:10 +0800

    Add test for length of output array

--- test/distributeMoney.test.js
+++ test/distributeMoney.test.js
@@ -1,6 +1,13 @@
 const distr = require('../src/distributeMoney');
 
 describe('distributeMoney Tests', () => { 
+    test('correct number of participants in output', () => {
+        for (let i = 1; i < 20; i++) {
+            let distribution = distr.distributeMoney(i, 10);
+            expect(distribution.length).toEqual(i);
+        }
+    })
+
     test('sum of distribution equals to input amount', () => {
         for (let amount = 1; amount <= 100; amount += 10) {
             for (let participants = 1; participants <= 10; participants++) {
@@ -10,6 +17,4 @@ describe('distributeMoney Tests', () => {
             }
         }
     })
-
-    
 })
